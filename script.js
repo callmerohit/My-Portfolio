8529933f671093faf0259c5e5f19ac26232bdf94
Rohit Kumar <144269961+callmerohit@users.noreply.github.com>
2024-02-01 23:38:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let modeButton = document.querySelector("#modeButton");
+let resumeBtn=document.querySelector(".resumeBtn");
+
+let mode="lightMode";
+const toggleDarkmode =()=>{  // DARKMODE
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelectorAll(".bg-wp").forEach((bgwp)=>{
+        bgwp.style.backgroundColor = "#560bad"; // Purple
+    })
+    document.querySelectorAll(".col-pw").forEach((colpw)=>{
+        colpw.style.color=  "#ffffff" // white
+    })
+    document.querySelectorAll(".col-gw").forEach((colgw)=>{
+        colgw.style.color = "#ffffff" // white
+    })
+    document.querySelector(".navbar").style.boxShadow = "0 0 10px rgb(236, 231, 231, 0.9)"
+    document.querySelector("#modeButton").style.backgroundColor= "#ffffff"; // White
+    document.querySelector("#modeButton").innerText="\u2600";
+};
+
+const toggleLightmode=()=>{  //LIGHTMODE
+    document.querySelector("body").style.backgroundColor = "rgb(236, 231, 231)";//smokyWhite
+    document.querySelectorAll(".bg-wp").forEach((bgwp)=>{
+        bgwp.style.backgroundColor =  "#ffffff"; // White
+    })
+    document.querySelectorAll(".col-pw").forEach((colpw)=>{
+        colpw.style.color ="#560bad"; // Purple
+})
+    document.querySelectorAll(".col-gw").forEach((colgw)=>{
+    colgw.style.color ="#353535" //Grey
+})
+    document.querySelector(".navbar").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
+    document.querySelector("#modeButton").style.backgroundColor="#560bad"; // Purple
+    document.querySelector("#modeButton").innerText="\uD83C\uDF19";
+};
+
+// Contact Me function
+function  openMailBox() {
+    const myEmailAddress = "rohitkumarr2212@gmail.com" 
+    // Create a mailto + encodeURIComponent
+    const linkToMailbox = "mailto:" + encodeURIComponent(myEmailAddress);
+    // Open the default emailSoftware
+    window.location.href = linkToMailbox;
+};
+
+//myResume
+resumeBtn.addEventListener("click",()=>{
+    let myResume = "myResume.pdf";
+    window.open(myResume);
+});
+// colorMode Button
+modeButton.addEventListener("click",()=>{
+    if(mode==="lightMode"){
+        mode="darkMode";
+        toggleDarkmode();
+    }
+    else{
+        mode="lightMode";
+        toggleLightmode();
+    }
+    // console.log(mode);
+});
+// smoothScrolling
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if(targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
+  
